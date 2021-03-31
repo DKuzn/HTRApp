d037fd7a1adc2ef1646afcf769df7237c8c79544
Dmitry Kuznetsov <DKuznetsov2000@mail.ru>
2021-03-31 19:43:56 +0300

    Update paths

--- src/train.cpp
+++ src/train.cpp
@@ -3,15 +3,15 @@
 #include <iostream>
 
 int main() {
-    std::string path = "../train.csv";
+    std::string path = "./train.csv";
     Dataset ds = Dataset(path);
     std::vector<unsigned int> topology = {784, 120, 10};
     NeuralNetwork net = NeuralNetwork(topology, 0.0001);
-    for (int j = 0; j < 2; j++) {
+    for (int j = 0; j < 10; j++) {
         for (int i = 0; i < 42000; i++) {
             net.train({ds[i].x}, {ds[i].y});
         }
-        net.saveWeights("../HTRApp/net_weights.csv");
+        net.saveWeights("./net_weights.csv");
     }
 }
 
